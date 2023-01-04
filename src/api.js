073eb2b19d905202e7e04ded670bd9790f13b157
Maxvel <maximusfullpro1@gmail.com>
2023-01-04 20:21:28 +0200

    first commit

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'http://api.template.maxvel.pp.ua'
 import router from "@/router";
 
 const api = axios.create();
