63357a6db70cbe4072907c00f6c5f828494a8308
Maxvel <maximusfullpro1@gmail.com>
2023-01-24 15:57:09 +0200

    commit tag count fix

--- src/views/app/shop/PartsComponent.vue
+++ src/views/app/shop/PartsComponent.vue
@@ -193,7 +193,7 @@
               <h3 class="mt30">Фільтр</h3>
               <div class="widget_list">
                 <ul v-for="tag in tags" class="list_details">
-                  <input type="checkbox" v-model="tagsFilter" :value="tag.id"> <span v-if="tag.count">{{ tag.title }} {{'(' + tag.count + ')' }}</span>
+                  <input v-if="tag.count" type="checkbox" v-model="tagsFilter" :value="tag.id"> <span v-if="tag.count">{{ tag.title }} {{'(' + tag.count + ')' }}</span>
                 </ul>
               </div>
             </div>
